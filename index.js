77e583b3b2985c0fa9359255a2dffa0e0e162f99
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-04 20:33:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const driversByName = function (drivers) {
 };
 
 const totalRevenue = function(drivers) {
-  
-}
+  return drivers.reduce(function (total, driver) {
+    return driver.revenue + total;
+  }, 0);
+};
 
 function averageRevenue() {}
